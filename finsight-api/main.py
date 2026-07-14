3e62d2cfec70695b35c96aa2b1138d6162a2ac6d
siyantech <siyantechglobal@gmail.com>
2026-07-14 20:17:12 +0530

    Add Vercel domain to CORS

--- finsight-api/main.py
+++ finsight-api/main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:3001", "http://localhost:3002"],
+    allow_origins=["http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "https://*.vercel.app", "https://finsight-web.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
